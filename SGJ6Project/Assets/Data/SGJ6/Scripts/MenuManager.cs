0da26f8432fabf3aaafbdc7661b0aee1655b2635
Martin Prado <Martin Prado>
2016-06-04 17:21:20 +0200

    Menu + Prefabs Menu

---
+++ SGJ6Project/Assets/Data/SGJ6/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public void BeginButton () {
+
+		Debug.Log ("Begin Game");
+
+		BeginGame ();
+
+	}
+
+	public void QuitButton () {
+
+		Debug.Log ("Quit game");
+
+		Application.Quit ();
+
+	}
+
+	void BeginGame () {
+
+		Debug.Log ("Start Game");
+
+	}
+
+}
