54303d06fdd9c874251022c7f370363ddabfdb0b
Daniel Parra <dani.parra.90@gmail.com>
2016-06-04 13:44:16 +0200

    + Breakable

---
+++ SGJ6Project/Assets/Data/SGJ6/Scripts/Level/Breakable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PONME_NAMESPACE {
+
+	public class Breakable : MonoBehaviour {
+
+        [SerializeField]
+        private int maxLife = 1;
+        public int CurrentLife
+        {
+            get { return maxLife; }
+        }
+
+	}
+}
