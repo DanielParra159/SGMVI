ef839dc72f519358d6720cf0868d001886bff578
Martin Prado <Martin Prado>
2016-06-04 12:39:22 +0200

    Script character

--- SGJ6Project/Assets/Data/SGJ6/Scripts/Character.cs
+++ SGJ6Project/Assets/Data/SGJ6/Scripts/Character.cs
@@ -1,33 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 using SGJVI.Level;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets._2D;
 
 namespace SGJVI.Characters
+
 {
-    public class Character : MonoBehaviour
+	[RequireComponent(typeof (PlatformerCharacter2D))]
+	public class Character : MonoBehaviour
     {
 
+		private PlatformerCharacter2D m_Character;
+		private bool m_Jump;
+		private float direction;
+		private bool crouch;
 
-
+		private void Awake()
+		{
+			m_Character = GetComponent<PlatformerCharacter2D>();
+			direction = -1.0f;
+		}
 
 		void Start()
         {
             LevelManager.Instance.MainCharacter = this;
         }
 			
-        void Update()
-        {
 
-        }
+		private void FixedUpdate()
+		{
+			// Read the inputs.
+
+
+			// Pass all parameters to the character control script.
+			m_Character.Move(direction, crouch, m_Jump);
+			m_Jump = false;
+		}
 
         public void ChangeDirection(InputCharacter.SwipeDirection dir)
         {
             Debug.Log("ChangeDirection " + dir);
+
+			if (dir == InputCharacter.SwipeDirection.Left) {
+
+				direction = -1.0f;
+
+			} else {
+
+				direction = 1.0f;
+
+			}
+
+
         }
 
         public void Jump()
         {
             Debug.Log("Jump");
+			m_Jump = true;
+
         }
     }
 }
